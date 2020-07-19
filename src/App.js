091780bb1309a8887859daad0a583c2109aad387
lcamps88 <lisandra.camps@gmail.com>
2020-07-18 23:30:42 -0400

    Recipies3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import FoodRecipies from './FoodRecipe/Food'
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
     <div >
-      <h1>Hello World</h1>
+      <Container>
+        <FoodRecipies/>
+      </Container>
+      
     </div>
   );
 }
